2c12383fd7b7b6523e4c5ea118a80ecfe71baa5e
Kat <karacav@msn.com>
2022-01-03 20:03:21 -0800

    Update january.js

--- january.js
+++ january.js
@@ -5,7 +5,9 @@ var twoSum = function(nums, target) {
     for(let x = 0; x < nums.length; x++) {
         for (let y = 0; y < nums.length; y++) {
             if(nums[x] + nums[y] == target) {
-                return [x, y];
+                if(x != y) {
+                    return [x, y];
+                }
             }
         }
     }
